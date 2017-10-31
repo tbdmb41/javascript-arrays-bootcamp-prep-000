277652b5cad7a51100c560fd6775907b9cf726e3
Thomas Bowen <tbowen1@outlook.com>
2017-10-31 17:16:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,46 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles",];
-  chocolateBars.push("snickers")
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
 
-var myArray =["snickers", "hundred grand", "kitkat", "skittles"]
-myArray[snickers] = 5 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
