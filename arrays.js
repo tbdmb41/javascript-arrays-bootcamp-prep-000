db67e5e21e7aa8fe48b6665520b135f9998e0990
Thomas Bowen <tbowen1@outlook.com>
2017-10-31 16:50:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles",];
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles",];
   var chocolateBars.push("kitkat")
 
-chocolateBars.unshift("snickers")
+var chocolateBars.unshift("snickers")
